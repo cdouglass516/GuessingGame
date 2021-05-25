df2af1c632c2ec3b47bc123896f4bb8464b743a2
Chris Douglass <cdouglass516@gmail.com>
2021-05-25 11:20:48 -0500

    first phase commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+    Console.WriteLine("Hello World!");
+    main();
+    void askUser()
+    {
+        Console.WriteLine("|-------------------------------------------------------------------|");
+        Console.WriteLine("|----   Please Guess the secret number :       ---------------------|");
+        Console.Write("|       Your Guess Is:");
+        string playerResponse = Console.ReadLine();
+        int playerNumber;
+        if(Int32.TryParse(playerResponse, out playerNumber))
+        {
+            Console.Write("|----   The number you guessed is :" + playerNumber.ToString());
+            Console.Write ("  ");
+            for(int i = 0; i < (31 - playerNumber.ToString().Length); i ++){
+                Console.Write ('-');
+            }
+            Console.Write ('|');
+            Console.WriteLine();
+        }
+        else{
+              Console.WriteLine("|----   It has to be a number :       ------------------------------|");
+              askUser();
+        }
+
+    }
+    void main(){
+        askUser();
+
+    }
